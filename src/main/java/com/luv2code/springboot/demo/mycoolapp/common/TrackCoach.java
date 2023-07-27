0de50de8abd9dc131587566070b078073984671e
Eduardo <40169421+skullino@users.noreply.github.com>
2023-07-27 16:49:38 -0300

    Added @Primary; Configuration for global Lazy;

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TrackCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TrackCoach.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.demo.mycoolapp.common;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary
 public class TrackCoach implements Coach {
     @Override
     public String getDailyWorkout() {
