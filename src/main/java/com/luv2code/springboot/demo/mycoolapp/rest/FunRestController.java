1cf02a047b11007e17f47643e38e4e217f09273d
Eduardo <40169421+skullino@users.noreply.github.com>
2023-07-27 14:33:50 -0300

    Add @Values;

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,16 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
+
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World!";
@@ -18,5 +24,10 @@ public class FunRestController {
 	@GetMapping("/fortune")
 	public String getDailyFortune() {
 		return "Today is your lucky day.";
+	}
+
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team: " + teamName;
 	}
 }
